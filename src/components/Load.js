cede5e0dc0129b958e2c6e1e9e603b475ef92ac7
Maciej Szczepanski <maciek2712@interia.pl>
2020-05-02 17:12:33 +0200

    add spiner

--- src/components/Load.js
+++ src/components/Load.js
@@ -1,50 +1,8 @@
-import React, { Component } from "react";
-import Circle from "../class/Circle";
+import React from "react";
 import "../styles/Load.scss";
 
-class Load extends Component {
-  constructor(props) {
-    super(props);
-    this.stopId = "";
-    this.circles = [];
-  }
-
-  componentDidMount() {
-    //setup canvas
-    const canvas = document.querySelector(".load__canvas");
-    const c = canvas.getContext("2d");
-    canvas.width = 300;
-    canvas.height = 300;
-
-    //create circles
-    const color = "#262626";
-    const radius = 2;
-    const quantity = 25;
-    const coor = 150;
-
-    for (let i = 0; i < quantity; i++) {
-      const direction = Math.random() * 2 - 1;
-      this.circles.push(new Circle(c, coor, coor, radius, color, direction));
-    }
-    //run animation
-    const animation = () => {
-      this.stopId = requestAnimationFrame(animation);
-      c.fillStyle = "rgba(251, 251, 251,0.1)";
-      c.fillRect(0, 0, canvas.width, canvas.height);
-      this.circles.forEach((circle) => circle.update());
-    };
-    animation();
-  }
-  componentWillUnmount() {
-    cancelAnimationFrame(this.stopId);
-  }
-  render() {
-    return (
-      <div className="load">
-        <canvas className="load__canvas"></canvas>
-      </div>
-    );
-  }
-}
+const Load = () => {
+  return <div className="triple-spinner"></div>;
+};
 
 export default Load;
