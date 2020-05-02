816bc32fb9829fc4d9cba72072fde2e0ee04f5cd
Maciej Szczepanski <maciek2712@interia.pl>
2020-05-02 16:31:13 +0200

    add .env & updae log panel

--- src/components/PanelLog.js
+++ src/components/PanelLog.js
@@ -4,13 +4,14 @@ import InputUser from "./InputUser";
 import InputPassword from "./InputPassword";
 import IncorrectTxt from "./IncorrectTxt";
 import "../styles/PanelLog.scss";
+require("dotenv").config();
 
 const PanelLog = ({ click }) => {
-  const user = useRef(null);
-  const password = useRef(null);
+  const inputUser = useRef(null);
+  const inputPassword = useRef(null);
 
-  const [correctUser, setCorrectUser] = useState(true);
-  const [correctPassword, setCorrectPassword] = useState(true);
+  const [userOk, setUserOk] = useState(true);
+  const [passwordOk, setPasswordOk] = useState(true);
 
   const infoTxt = useMemo(() => {
     return {
@@ -18,36 +19,30 @@ const PanelLog = ({ click }) => {
       password: "Podano błędne hasło. Spróbuj jeszcze raz.",
     };
   }, []);
+
   const handleSubmit = useCallback((e) => {
     e.preventDefault();
-    const db = firebase.database();
-    const ref = db.ref();
-    //get password from db & make validation
-    ref.once("value", (snapshot) => {
-      const passwordPattern = new RegExp(`^${snapshot.val().admin}$`);
-      const userPattern = /^admin$/;
-      const userTest = userPattern.test(user.current.value);
-      const passwordTest = passwordPattern.test(password.current.value);
+    const inputPasswordValue = inputPassword.current.value;
+    const inputUserValue = inputUser.current.value;
+    const ifUserOk = inputUserValue === process.env.REACT_APP_LOGIN;
+    const ifPasswordOk = inputPasswordValue === process.env.REACT_APP_PASSWORD;
 
-      if (userTest && passwordTest) {
-        click();
-      } else {
-        setCorrectUser(userTest);
-        setCorrectPassword(passwordTest);
-      }
-    });
+    if (ifUserOk && ifPasswordOk) {
+      click();
+    } else {
+      setUserOk(ifUserOk);
+      setPasswordOk(ifPasswordOk);
+    }
   }, []);
 
   return (
     <div className="log">
       <form onSubmit={handleSubmit} className="log__form">
-        <InputUser ref={user}>użytkownik</InputUser>
-        <IncorrectTxt correct={correctUser}>{infoTxt.user}</IncorrectTxt>
+        <InputUser ref={inputUser}>użytkownik</InputUser>
+        <IncorrectTxt correct={userOk}>{infoTxt.user}</IncorrectTxt>
 
-        <InputPassword ref={password}>hasło</InputPassword>
-        <IncorrectTxt correct={correctPassword}>
-          {infoTxt.password}
-        </IncorrectTxt>
+        <InputPassword ref={inputPassword}>hasło</InputPassword>
+        <IncorrectTxt correct={passwordOk}>{infoTxt.password}</IncorrectTxt>
 
         <input type="submit" value="zaloguj" className="log__btn" />
       </form>
